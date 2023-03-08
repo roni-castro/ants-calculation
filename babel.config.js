7790814c4d9d862843c6a2a8609a804c38093a0e
Roni Castro <roni.c.castro@gmail.com>
2023-03-08 15:13:44 -0300

    chore: config path alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components'
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      ]
+    ]
+  }
+}
